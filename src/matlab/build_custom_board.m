aedbdb39755d5a57814b0e4f8e1b863a117fb834
RubenPadialAllue <70911219+RPadial@users.noreply.github.com>
2025-10-26 08:06:30 +0100

    Reduce dlprocessor implementation by reducin Conv/FC Thread Number and changing precission from 'single' to 'int8'

--- src/matlab/build_custom_board.m
+++ src/matlab/build_custom_board.m
@@ -8,15 +8,34 @@
 % HDL Coder
 % Deep Learning HDL Toolbox
 % Deep Learning HDL Toolbox Support Package for Intel FPGA and SoC Devices
+% xterm
+% https://es.mathworks.com/matlabcentral/answers/1939799-deep-learning-hdl-toolbox-de10-standard
 
+% matlab -nodesktop -nosplash -nodesktop -r "build_custom_board"
+
+addpath(genpath('boards'))
 % Create the processor configuration object
 hPC = dlhdl.ProcessorConfig();
+hPC.setModuleProperty("conv","ConvThreadNumber",4)
+hPC.setModuleProperty("conv","InputMemorySize",[28 28 1])
+hPC.setModuleProperty("conv","OutputMemorySize",[10 10 1])
+hPC.setModuleProperty("conv","FeatureSizeLimit", 1024)
+
+hPC.setModuleProperty("fc","FCThreadNumber",4)
+hPC.setModuleProperty("fc","InputMemorySize",1024)
+hPC.setModuleProperty("fc","OutputMemorySize",1024)
+
+hPC.ProcessorDataType='int8'
+hPC.UseVendorLibrary ='off' % Option to use vendor-specific floating point libraries
 
 hPC.TargetPlatform = 'Terasic DE10-Nano SoC';
 
 % Add the synthesis tool to the MATLAB path. Uncomment the line below and
 % provide the correct path.
-hdlsetuptoolpath('ToolName', 'Altera Quartus II','ToolPath', '/opt/FPGA/Intel/intelFPGA/21.1std/quartus/bin/quartus');
+% quartus_path = strcat(getenv("QUARTUS_ROOTDIR"), "/bin/quartus");
+quartus_path = '/opt/FPGA/Intel/intelFPGA/21.1std/quartus/bin/quartus'
+
+hdlsetuptoolpath('ToolName', 'Altera Quartus II','ToolPath', quartus_path);
 
 
 % Create workflow config object
