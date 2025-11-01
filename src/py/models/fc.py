b7aba864f2be194098eba3065b1fe43782b9942f
RubenPadialAllue <70911219+RPadial@users.noreply.github.com>
2025-11-01 08:37:46 +0100

    Reduced first fc layer from 128 to 64 neurons.

--- src/py/models/fc.py
+++ src/py/models/fc.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 def build_fc_model(input_shape=(28, 28, 1), num_classes=10):
     model = tf.keras.Sequential([
         tf.keras.layers.Flatten(input_shape=input_shape),
-        tf.keras.layers.Dense(128),
+        tf.keras.layers.Dense(64),
         tf.keras.layers.ReLU(),
         tf.keras.layers.Dense(32),
         tf.keras.layers.ReLU(),
