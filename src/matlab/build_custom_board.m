b6f2f42eb5667a96fa2b86285c45bdd37cdfad5e
RubenPadialAllue <70911219+RPadial@users.noreply.github.com>
2025-11-04 09:22:24 +0100

    build_custom_board

--- src/matlab/build_custom_board.m
+++ src/matlab/build_custom_board.m
@@ -7,13 +7,35 @@
 % MATLAB
 % HDL Coder
 % Deep Learning HDL Toolbox
+% Deep Learning Toolbox Converter for TensorFlow Models
 % Deep Learning HDL Toolbox Support Package for Intel FPGA and SoC Devices
 % xterm
 % https://es.mathworks.com/matlabcentral/answers/1939799-deep-learning-hdl-toolbox-de10-standard
 
-% matlab -nodesktop -nosplash -nodesktop -r "build_custom_board"
+% matlab2024b -nodesktop -nosplash -nodesktop -r "build_custom_board"
+
+optimize_dlprocessor_to_nn = true;
+precision = 'single'; % 'sinole' or 'int0'
 
 addpath(genpath('boards'))
+
+%% Load model 
+model_path='saved_models/mnist_fc/model.h5'
+net = importKerasNetwork(model_path);
+info = analyzeNetwork(net)
+% Classification output
+correctClasses = string(0:9);
+% Get original layers
+layers = net.Layers;
+newClassificationLayer = classificationLayer('Classes', categorical(correctClasses), 'Name', 'output');
+
+% Replace output layer
+layers(end) = newClassificationLayer;
+
+% Assemply Network with modified output layer
+net = assembleNetwork(layers);
+info = analyzeNetwork(net)
+
 % Create the processor configuration object
 hPC = dlhdl.ProcessorConfig();
 hPC.setModuleProperty("conv","ConvThreadNumber",4)
@@ -25,9 +47,16 @@ hPC.setModuleProperty("fc","FCThreadNumber",4)
 hPC.setModuleProperty("fc","InputMemorySize",1024)
 hPC.setModuleProperty("fc","OutputMemorySize",1024)
 
-hPC.ProcessorDataType='int8'
-hPC.UseVendorLibrary ='off' % Option to use vendor-specific floating point libraries
+if optimize_dlprocessor_to_nn
+  hPC.optimizeConfigurationForNetwork(net);
+end
 
+if strcmp(precision,'int8')
+  hPC.ProcessorDataType='int8'
+  hPC.UseVendorLibrary ='off' % Option to use vendor-specific floating point libraries
+end
+
+%% Define board
 hPC.TargetPlatform = 'Terasic DE10-Nano SoC';
 
 % Add the synthesis tool to the MATLAB path. Uncomment the line below and
@@ -36,7 +65,6 @@ hPC.TargetPlatform = 'Terasic DE10-Nano SoC';
 quartus_path = '/opt/FPGA/Intel/intelFPGA/21.1std/quartus/bin/quartus'
 
 hdlsetuptoolpath('ToolName', 'Altera Quartus II','ToolPath', quartus_path);
-
 
 % Create workflow config object
 hWC = hdlcoder.WorkflowConfig('SynthesisTool', hPC.SynthesisTool,'TargetWorkflow','Deep Learning Processor');
